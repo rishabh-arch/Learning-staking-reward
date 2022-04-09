3604339a21df20631106c90a19a74ec513f29420
Rishabh <rishabhgargts@gmail.com>
2022-04-09 13:58:38 +0530

    done migration

---
+++ src/contracts/Tether.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Tether {
+    string public name = "Tether";
+    string public symbol = "USDT";
+    uint public decimals = 18;
+    uint public totalSupply = 1000000000000000000; //1 million tokens
+}
